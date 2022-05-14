943e514067121cfba7a7b2b6b200a8d890bd17ed
TheSilverKing <95306208+TheSilverKing@users.noreply.github.com>
2022-05-14 16:42:45 +0300

    Added Task 1

---
+++ ToUpper/ToUpper/ToUpper.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+char ToUpper(char input_symbol) {
+    int result_symbol;
+    if ((int)input_symbol > 96 && (int)input_symbol < 123) 
+    {
+        return input_symbol - 32;
+    }
+    return input_symbol;
+}
+
+int main()
+{
+    char sym;
+    std::cout << "Enter the letter: " << std::endl;
+    std::cin >> sym;
+    char result = ToUpper(sym);
+    std::cout << result << std::endl;
+}
+
